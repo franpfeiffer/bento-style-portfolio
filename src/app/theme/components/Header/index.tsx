52c5724d476b66fe4d66cd33f7de05ebc1c61a15
Francisco Pfeiffer <pfeifferfran1@gmail.com>
2024-03-16 10:09:54 -0300

    cv update

--- src/app/theme/components/Header/index.tsx
+++ src/app/theme/components/Header/index.tsx
@@ -39,7 +39,7 @@ const Header = () => {
 					)}
 					<a
 						className='w-auto h-7 px-4 font-medium flex items-center gap-2 bg-dusky text-moonlit rounded-3xl dark:text-dusky dark:bg-moonlit lg:h-10 lg:px-6 cursor-pointer active:scale-[1.1] transition-all duration-300'
-						href='https://drive.google.com/file/d/1Mx-aojdwj94er6y-cYiGKF66J27Xu7Ix/view'
+						href='https://drive.google.com/file/d/1YDoBaJk6r-3A9zIJjvf0vGnqiez6PyW7/view?usp=sharing'
 						target='_blank'
 					>
 						<GrCloudDownload />
